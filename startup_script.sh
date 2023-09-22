ea9fc360a0c923d1f4d8eff718cf282d4bd518dd
bAe <bjekele@fexco.com>
2023-09-22 20:59:33 +0100

    Update startup script

--- startup_script.sh
+++ startup_script.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-sudo yum update -y
-sudo yum install nginx -y
-
-sudo systemctl start nginx
-
-echo "<html><head><title>Bienvenue</title></head><body><h1>I am Gro0t</h1></body></html>" | sudo tee /usr/share/nginx/html/index.html
-
-sudo systemctl restart nginx
+sudo amazon-linux-extras install nginx1.12 -y
 
 sudo systemctl enable nginx
+
+sudo systemctl start nginx
