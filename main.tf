f0abbdc60bd1d3a4a02bb353bdcef98f59be89e5
Bissallah Junior Ekele <bisskay@gmail.com>
2023-09-22 15:57:10 +0100

    var test

--- main.tf
+++ main.tf
@@ -11,3 +11,13 @@ terraform {
 provider "aws" {
   region = "eu-west-1"
 }
+
+variable "my_ip" {
+  description = "(Required) Your IP address used for SSH access."
+  type        = string
+}
+
+variable "existing_key_name" {
+  description = "(Required) The name of an existing key pair."
+  type        = string
+}
